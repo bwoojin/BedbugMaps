1c9df7719bd0dcb8979b876b41c239d66bbe8b09
bwoojin <106170550+bwoojin@users.noreply.github.com>
2023-12-03 16:36:54 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+  $jb_conn = mysqli_connect( 'localhost', 'root', 'dnwls0528!', 'bedbugmaps' );
+  $sql = "SELECT * FROM bedbug_maps LIMIT 100;";
+  $result = mysqli_query( $conn, $sql );
+  while( $row = mysqli_fetch_array( $result ) ) {
+    echo
+    ''.$row['address'].';'.$row['lat'].';'.$row['lng'].':';
+    }
+?>
