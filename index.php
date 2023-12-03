aaa184a8edd1c652e9319d9659894c466ebe13cd
bwoojin <106170550+bwoojin@users.noreply.github.com>
2023-12-03 19:08:13 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-
   $jb_conn = mysqli_connect( 'localhost', 'root', 'dnwls0528!', 'bedbugmaps' );
   $sql = "SELECT * FROM bedbug_maps LIMIT 100;";
-  $result = mysqli_query( $conn, $sql );
+  $result = mysqli_query( $jb_conn, $sql );
   while( $row = mysqli_fetch_array( $result ) ) {
     echo
     ''.$row['address'].';'.$row['lat'].';'.$row['lng'].':';
