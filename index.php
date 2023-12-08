7482c34e418e076af118878a9e1d92b314d5f09a
bwoojin <106170550+bwoojin@users.noreply.github.com>
2023-12-09 00:40:36 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
   $jb_conn = mysqli_connect( 'svc.sel4.cloudtype.app:32482', 'root', 'dnwls0528!', 'bedbugmaps' );
   $sql = "SELECT * FROM bedbug_maps LIMIT 100;";
   $result = mysqli_query( $jb_conn, $sql );
